7e286c0ebd7c2b85090c83a95309bea4de0b44cc
Python <diegorscorpion@gmail.com>
2019-05-01 17:40:57 -0500

    .

--- Página web/templates/signup.php
+++ Página web/templates/signup.php
@@ -49,13 +49,14 @@
 
     <body>
 
-        <?php if(isset($_POST['form_submitted'])): ?>
+        <?php if(isset($_POST['submit'])): ?>
 
-            <h2>Gracias <?php echo $_POST['nombre']; ?></h2>
+            <h3>Registro exitoso, bienvenido <?php echo $_POST['nombre']; ?></h3>
+            <br>
 
-            <!--<?php
+            <?php
 
-                /*$mysqli = new mysqli('localhost', 'id9431753_admin', 'nasa254', 'id9431753_nasa');
+                $mysqli = new mysqli('localhost', 'id9431753_admin', 'nasa254', 'id9431753_nasa');
                                 
                 if(!$mysqli)
                 {
@@ -66,8 +67,8 @@
                                 
                 //echo 'Conectado satisfactoriamente';
 
-                //if(isset($_POST['submit']))
-                //{
+                if(isset($_POST['submit']))
+                {
 
                     $nombre = $_POST["nombre"];
                     $email = $_POST["email"];
@@ -75,18 +76,22 @@
 
                     echo '<script>alert("Hola '. $nombre . ' ' . $email . ' ' . $password .'");</script>';
 
-                    /*$sql = "INSERT INTO usuarios(nombre, email, password, credito, rol) VALUES (".$nombre.", ".$email.", ".$password .", 0, \"USUARIO\")";
+                    $sql = "INSERT INTO usuarios (nombre, email, password, credito, rol) VALUES ('".$nombre."', '".$email."', '".$password ."', 0, 'USUARIO')";
 
                     if($mysqli-> query($sql))
                     {
 
                         echo '<script>alert("Registro existoso");</script>';
 
-                    }*/
+                    }
 
-                //}*/
+                }
 
-            ?>-->
+            ?>
+
+        <?php endif; ?>
+
+        <a href="/">Regresar a inicio</a>
 
     </body>
 
